53bbfe0f5411409c7cdd23e825d361ba5fbdc108
YaroslavLtv <slavik.ltv@gmail.com>
2021-03-26 19:01:54 +0200

    1

--- Python02_lesson1_2/main.py
+++ Python02_lesson1_2/main.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
+
+articles = [
+    {
+        'id': 1,
+        'author': 'Petro Petrovich',
+        'title': 'Hello world!',
+        'text': 'Hello world weqeqweqweHello world weqeqweqweHello world weqeqweqweHello world weqeqweqweHello world weqeqweqwe'
+    },
+    {
+        'id': 2,
+        'author': 'Ivan Ivanovich',
+        'title': 'World Hello',
+        'text': 'World HelloWorld HelloWorld HelloWorld HelloWorld HelloWorld HelloWorld HelloWorld HelloWorld HelloHelloWorld HelloHelloWorld HelloHelloWorld HelloHelloWorld Hello'
+    }
+]
 
 
 @app.route('/')
 def main_page():
-    return render_template('index.html', title='ItStep Blog')
+    return render_template('index.html', title='ItStep Blog', articles=articles)
+
+
+@app.route('/article/<int:id>')
+def get_article(id):
+    for article in articles:
+        if article['id'] == id:
+            return render_template('generic.html', article=article)
+    abort(404)
 
 
 @app.route('/about')
